5a947fc8e083f5f4bfbc91653a7c0a09ace4e95f
jndro <jndro.retana@gmail.com>
2020-08-11 19:17:24 -0500

    COnfiguración de un proyecto de reactjs usando webpack. Creación de los primeros componentes del proyecto platzi video (Header, Search)

---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../components/Header";
+import { Search } from "../components/Search";
+
+import "../assets/styles/App.scss";
+
+export const App = () => (
+  <>
+    <Header />;
+    <Search />;
+  </>
+);
